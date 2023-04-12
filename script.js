f861d5b260530e23abf916244ed6e2c302a0c565
Derik Nascimento <derikwnacimento@gmail.com>
2023-04-12 20:14:36 -0300

    feature: novos scripts js !

---
+++ script.js
@@ -0,0 +1,7 @@
+let input1 = parseInt(document.getElementById("campo-a").value)
+let input2 = parseInt(document.getElementById("campo-b").value)
+
+let botaoValidacao = document.getElementsByClassName('blt')
+botaoValidacao.addEventListener('click', function(e){
+
+})
