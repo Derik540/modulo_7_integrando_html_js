3f529b11a1ba93c3a293976d80bd398b4b0b7bb8
Derik Nascimento <derikwnacimento@gmail.com>
2023-04-15 20:45:07 -0300

    feature: adicionado feedback estilizado

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
-let input1 = parseInt(document.getElementById("campo-a").value)
-let input2 = parseInt(document.getElementById("campo-b").value)
+let botao = document.querySelector('#btl');
+let feedback = document.querySelector('#feedback');
 
-let botaoValidacao = document.getElementsByClassName('blt')
-botaoValidacao.addEventListener('click', function(e){
+botao.addEventListener('click', function(event) {
+    event.preventDefault();
 
-})
+    let inputA = document.querySelector('#campo-a');
+    let inputB = document.querySelector('#campo-b');
+    let valorA = parseInt(inputA.value);
+    let valorB = parseInt(inputB.value);
+
+    if (valorB >= valorA) {
+        
+        let mensagem = document.createElement('p');
+        mensagem.innerText = 'mensagem validada com sucesso';
+        mensagem.classList.add('feedback-validado')
+        feedback.innerHTML = '';
+        mensagem.style.color = 'green'
+        feedback.appendChild(mensagem);
+    } else {
+        
+        let mensagem = document.createElement('p');
+        mensagem.innerText = 'Valor de B presisa ser maior que A !';
+        mensagem.classList.add('feedback-validado')
+        feedback.innerHTML = '';
+        mensagem.style.color = 'red'
+        feedback.appendChild(mensagem);
+    }
+});
