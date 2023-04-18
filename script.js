eab5ea4af172c17befed921acfc42679d5a99005
Derik Nascimento <derikwnacimento@gmail.com>
2023-04-18 17:42:12 -0300

    FEATURE: agora se um o campo não ser preenchido será exibido uma mensagem avisando

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 let botao = document.querySelector('#btl');
+let tipbtl = document.querySelector('#tip')
 let feedback = document.querySelector('#feedback');
+
 
 botao.addEventListener('click', function (event) {
     event.preventDefault();
 
-    let inputA = document.querySelector('#campo-a');
+let inputA = document.querySelector('#campo-a');
     let inputB = document.querySelector('#campo-b');
     let valorA = parseInt(inputA.value);
     let valorB = parseInt(inputB.value);
 
+    if (inputA.value.trim() === '' || inputB.value.trim() === ''){
+        window.alert('Preencha ambos campos !')
+        return
+    }
+   
     if (valorB >= valorA) {
         inputA.classList.remove('input-invalido')
         inputB.classList.remove('input-invalido')
@@ -26,7 +33,7 @@ botao.addEventListener('click', function (event) {
         inputA.classList.remove('input-valido')
         inputB.classList.remove('input-valido')
         let mensagem = document.createElement('p');
-        mensagem.innerText = 'Valor de B presisa ser maior que A !';
+        mensagem.innerText = 'Valor de B precisa ser maior que A !';
         mensagem.classList.add('feedback-validado')
         feedback.innerHTML = '';
         mensagem.style.color = 'red'
@@ -35,4 +42,8 @@ botao.addEventListener('click', function (event) {
         feedback.appendChild(mensagem)
     }
 });
+
+tipbtl.addEventListener('click', function (e) {
+    window.alert('Como funciona: O campo B sempre deve ser maior que A, caso contrario será exibido uma mensagem ')
+})
 
